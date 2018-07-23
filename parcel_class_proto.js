71e7898f525cd5d4851caee58d3748834d3f1c3e
Connor Gooding <connor.gooding@gamut.com>
2018-07-23 09:44:47 -0500

    Scaled out to 13.5k addresses and fixed CSV output to handle suites

--- parcel_class_proto.js
+++ parcel_class_proto.js
@@ -1,72 +1,4 @@
 /*************************************************************************/
-/* GLOBALS (Placeholders for a real back-end.) ***************************/
-/*************************************************************************/
-var coords = [[
-	new google.maps.LatLng(39.921897, -82.962416),
-	new google.maps.LatLng(39.922391, -82.962373),
-	new google.maps.LatLng(39.922439, -82.963370),
-	new google.maps.LatLng(39.921948, -82.963413),
-	new google.maps.LatLng(39.921897, -82.962416),
-	],[
-	new google.maps.LatLng(47.803279, -122.326666),
-	new google.maps.LatLng(47.804580, -122.326658),
-	new google.maps.LatLng(47.804587, -122.327353),
-	new google.maps.LatLng(47.803286, -122.327359),
-	new google.maps.LatLng(47.803279, -122.326666),
-	],[
-	new google.maps.LatLng(42.212959, -88.066620),
-	new google.maps.LatLng(42.213960, -88.066607),
-	new google.maps.LatLng(42.213436, -88.067713),
-	new google.maps.LatLng(42.212961, -88.067096),
-	new google.maps.LatLng(42.212959, -88.066620),
-	],[
-	new google.maps.LatLng(34.243214, -81.611637),
-	new google.maps.LatLng(34.243998, -81.611462),
-	new google.maps.LatLng(34.244058, -81.614461),
-	new google.maps.LatLng(34.243268, -81.614586),
-	new google.maps.LatLng(34.243214, -81.611637),
-	],[
-	new google.maps.LatLng(45.785694, -108.509095),
-	new google.maps.LatLng(45.785908, -108.508644),
-	new google.maps.LatLng(45.785961, -108.508700),
-	new google.maps.LatLng(45.786132, -108.508872),
-	new google.maps.LatLng(45.785917, -108.509314),
-	new google.maps.LatLng(45.785694, -108.509095),
-	]];
-
-var centers = [[39.921897, -82.962416],
-			   [47.803279, -122.326666],
-			   [42.212959, -88.066620],
-			   [34.243214, -81.611637],
-			   [45.785694, -108.509095]];
-
-var num = 0;
-
-var dunsNos = ['109506258', 
-			   '010211449',
-			   '007180253',
-			   '602642647',
-			   '048539977'];
-
-var addr1 = ['1221 MARION RD', 
-			   '6825 216TH ST SW STE E9',
-			   '885 TELSER RD',
-			   '7843 SC HIGHWAY 395',
-			   '501 N 28TH ST'];
-
-var addr2 = ['COLUMBUS, OH', 
-			   'LYNNWOOD, WA',
-			   'LAKE ZURICH, IL	',
-			   'NEWBERRY, SC',
-			   'BILLINGS, MT'];
-
-var classifications = ['NOT CLASSIFIED', 'NOT CLASSIFIED', 'NOT CLASSIFIED', 'NOT CLASSIFIED', 'NOT CLASSIFIED'];
-
-/*************************************************************************/
-/*************************************************************************/
-
-
-/*************************************************************************/
 /* FUNCTIONS *************************************************************/
 /*************************************************************************/
 
@@ -74,7 +6,7 @@ var classifications = ['NOT CLASSIFIED', 'NOT CLASSIFIED', 'NOT CLASSIFIED', 'NO
  * This function sets the Google map given an index for the data frame.
  */
 function setMap(num) {
-	var centerlatlng = new google.maps.LatLng(centers[num][0], centers[num][1]);
+	var centerlatlng = parcels[num].map_center;
 	var myOptions = {
 		zoom: 18,
 		center: centerlatlng,
@@ -82,13 +14,14 @@ function setMap(num) {
 	};
 	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 	//document.getElementById("duns_number").innerHTML = 'DUNS Number: ' + dunsNos[num];
-	document.getElementById("address1").innerHTML = addr1[num];
-	document.getElementById('address2').innerHTML = addr2[num];
+	document.getElementById("address1").innerHTML = parcels[num].addr1;
+	document.getElementById('address2').innerHTML = parcels[num].addr2;
+	//document.getElementById('map_number').innerHTML = num;
 
 	var Path = new google.maps.Polyline({
 		clickable: false,
 		geodesic: true,
-		path: coords[num],
+		path: parcels[num].perimeter,
 		strokeColor: "#FF0000",
 		strokeOpacity: 1.000000,
 		strokeWeight: 5
@@ -106,8 +39,8 @@ function initialize() {
 
 	// Here is pre-processing...
 
-	// Then load the first map.
-	setMap(0);
+	// Then load the first unclassified map.
+	setMap(num);
 };
 
 /* nextMap()
@@ -120,10 +53,11 @@ function submit() {
 	if (verdict == null) {
 		return;
 	} else {
-		classifications[num] = verdict;
+		parcels[num].classification = verdict;
+		parcels[num].chk = true;
 	}
 	clearForm();
-	num = (num + 1) % centers.length;
+	num = (num + 1) % parcels.length;
 	setMap(num);
 
 };
@@ -193,8 +127,9 @@ function classifyParcel() {
 
 function exportCSV() {
 	let csvContent = "data:text/csv;charset=utf-8,";
-	for(i = 0; i < coords.length; i++) {
-		csvContent += dunsNos[i] + ', ' + addr1[i] + ', ' + addr2[i] + ', ' + classifications[i] + '\r\n';
+	for(i = 0; i < parcels.length; i++) {
+		csvContent += parcels[i].duns_number + ', '
+		csvContent += parcels[i].addr1.replace(/,/g, "") + ', ' + parcels[i].addr2 + ', ' + parcels[i].classification + '\r\n';
 	}
 
 	var encodedUri = encodeURI(csvContent);
@@ -209,9 +144,5 @@ function exportCSV() {
 		document.getElementById('form_canvas').appendChild(link);
 	}
 };
-
-
-
 /*************************************************************************/
 /*************************************************************************/
-
